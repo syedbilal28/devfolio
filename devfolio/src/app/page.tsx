55b3d8ed1bb92651c47a130263c803630c5748a4
syedali28 <syed@meistery.net>
2024-03-16 20:49:09 +0500

    added background animations and image and corrected alignment

--- devfolio/src/app/page.tsx
+++ devfolio/src/app/page.tsx
@@ -3,7 +3,7 @@ import { Projects } from '@/components/projects/projects'
 import { SkillsBar } from '@/components/skills/skills'
 import { SocialLinks } from '@/components/socials/social-links'
 import InfoSideBar from '@/components/ui/info-bar'
-import { Flex, Layout, Menu } from 'antd'
+import { Col, Flex, Layout, Menu, Row } from 'antd'
 import Sider from 'antd/es/layout/Sider'
 import { Content } from 'antd/es/layout/layout'
 import Image from "next/image";
@@ -11,29 +11,43 @@ import Image from "next/image";
 export default function Home() {
   return (
     <Layout style={{ "padding": "25px" }} className='home-page'>
-        <Sider width={220} className='home-page__sider'>
-          <Flex justify="center" align="center" vertical style={{"height": "100%"}}>
-            <InfoSideBar/>
-          </Flex>
-        </Sider>
-        <Content>
-          <Flex justify='center' align='center' vertical>
-            <Heading/>
-            <Image 
-            src="/image 5.png"
-            width="750"
-            height="100"
-            alt="demo"
-            />
-          </Flex>
-          <SocialLinks/>
-          <Projects/>
-        </Content>
-        <Sider width={250}>
-          <Flex justify="center" align="center" vertical style={{"height": "100%"}}>
-            <SkillsBar/>
-          </Flex>
-        </Sider>
+      <section className="sticky">
+          <div className="bubbles">
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+            <div className="bubble"></div>
+          </div>
+        </section>
+            <Sider width={220} className='home-page__sider'>
+              <Flex justify="center" align="center" vertical style={{"height": "100%"}}>
+                <InfoSideBar/>
+              </Flex>
+            </Sider>
+            <Content>
+              <Flex justify='center' align='center' vertical style={{"height": "100%"}}>
+                <Heading/>
+                <Image
+                src="/image 5.png"
+                width="750"
+                height="100"
+                alt="demo"
+                />
+                <SocialLinks/>
+                <Projects/>
+              </Flex>
+            </Content>
+            <Sider width={250}>
+              <Flex justify="center" align="center" vertical style={{"height": "100%"}}>
+                <SkillsBar/>
+              </Flex>
+            </Sider>
     </Layout>
   )
 }
