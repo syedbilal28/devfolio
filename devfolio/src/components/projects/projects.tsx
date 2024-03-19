308c1e55f90ce3653bff8515ee7f2367772a3055
syedali28 <syed@meistery.net>
2024-03-20 02:47:29 +0500

    added two projects and scroling for projects

--- devfolio/src/components/projects/projects.tsx
+++ devfolio/src/components/projects/projects.tsx
@@ -6,7 +6,7 @@ export const Projects: React.FC = () => {
     return (
         <div className="projects">
             <h2 className="title">Projects</h2>
-            <Flex gap="middle" align="start" wrap="wrap">
+            <Flex gap="large" align="center" justify="center" wrap="wrap">
                 <Project
                     title="GlobalFleet Directory"
                     imageUrl="/global-fleet.png"
@@ -18,6 +18,18 @@ export const Projects: React.FC = () => {
                     imageUrl="/dater.png"
                     content="Implemented frontend and backend of the application integrating Geo location of users"
                     skills={["Python", "Django", "JavaScript", "Django-Rest-Framework", "Database Architecure", "PostgreSQL", "GDAL", "OAuth"]}
+                />
+                <Project
+                    title="Enchantiom"
+                    imageUrl="/enchantiom.png"
+                    content="Implemented frontend and backend of the application to let users maintain a log of their crypto transactions"
+                    skills={["Python", "Django", "JavaScript", "Django-Rest-Framework", "Database Architecure", "PostgreSQL"]}
+                />
+                <Project
+                    title="Uno Startup"
+                    imageUrl="/uno-startup.png"
+                    content="Designed, implemented and developed a scoial media application that allowed entrepreneurs to share their ideas"
+                    skills={["Python", "Django", "JavaScript", "Django-Rest-Framework", "Database Modelling", "PostgreSQL", "django-channels", "stripe integration", "redis"]}
                 />
             </Flex>
         </div>
